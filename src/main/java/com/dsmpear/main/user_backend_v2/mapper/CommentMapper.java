d470a00ea34b9d46280451627864c379e2c5e24d
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-22 21:20:25 +0900

    [ADD] CommentMapper

---
+++ src/main/java/com/dsmpear/main/user_backend_v2/mapper/CommentMapper.java
@@ -0,0 +1,25 @@
+package com.dsmpear.main.user_backend_v2.mapper;
+
+import com.dsmpear.main.user_backend_v2.entity.comment.Comment;
+import com.dsmpear.main.user_backend_v2.entity.language.Language;
+import com.dsmpear.main.user_backend_v2.entity.report.Report;
+import com.dsmpear.main.user_backend_v2.entity.user.User;
+import com.dsmpear.main.user_backend_v2.payload.request.CommentRequest;
+import com.dsmpear.main.user_backend_v2.payload.request.ReportRequest;
+import com.dsmpear.main.user_backend_v2.payload.response.ReportCommentsResponse;
+import com.dsmpear.main.user_backend_v2.payload.response.ReportContentResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(config = MapStructConfig.class)
+public interface CommentMapper {
+    @Mapping(source = "report", target = "report")
+    Comment requestToEntity(CommentRequest commentRequest, Report report, User user);
+
+    @Mapping(source = "comment.id", target = "commentId")
+    @Mapping(source = "comment.user.email", target = "userEmail")
+    @Mapping(source = "comment.user.name", target = "userName")
+    ReportCommentsResponse entityToResponse(Comment comment, Boolean isMine);
+}
