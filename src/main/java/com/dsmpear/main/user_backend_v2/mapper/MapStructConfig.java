003da32e3d40ef46a9866c98f44e271d74c80fcb
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-22 19:14:11 +0900

    [ADD] MapStruct

---
+++ src/main/java/com/dsmpear/main/user_backend_v2/mapper/MapStructConfig.java
@@ -0,0 +1,8 @@
+package com.dsmpear.main.user_backend_v2.mapper;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public class MapStructConfig {
+}
