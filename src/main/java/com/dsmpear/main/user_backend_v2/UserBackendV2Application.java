d582fc063342de926ef5de0a2c6c5628c4da3146
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-23 09:53:38 +0900

    [UPDATE] MainApplication

--- src/main/java/com/dsmpear/main/user_backend_v2/UserBackendV2Application.java
+++ src/main/java/com/dsmpear/main/user_backend_v2/UserBackendV2Application.java
@@ -2,7 +2,9 @@ package com.dsmpear.main.user_backend_v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class UserBackendV2Application {
 
