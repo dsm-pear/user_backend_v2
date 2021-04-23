202a3226ea4b6d37fec0b1b0579d14c71fb28a07
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-23 11:10:37 +0900

    [UPDATE] ReportController

--- src/main/java/com/dsmpear/main/user_backend_v2/controller/ReportController.java
+++ src/main/java/com/dsmpear/main/user_backend_v2/controller/ReportController.java
@@ -1,11 +1,40 @@
 package com.dsmpear.main.user_backend_v2.controller;
 
+import com.dsmpear.main.user_backend_v2.payload.request.ReportRequest;
+import com.dsmpear.main.user_backend_v2.payload.response.ReportContentResponse;
+import com.dsmpear.main.user_backend_v2.service.report.ReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/report")
 @RestController
 public class ReportController {
 
+    private final ReportService reportService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long writeReport(@RequestBody @Valid ReportRequest reportRequest) {
+        return reportService.createReport(reportRequest);
+    }
+
+    @GetMapping("/{reportId}")
+    public ReportContentResponse getReportContent(@PathVariable Long reportId) {
+        return reportService.getReport(reportId);
+    }
+
+    @PatchMapping("/{reportId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long updateReport(@PathVariable Long reportId, @RequestBody ReportRequest reportRequest) {
+        return reportService.updateReport(reportId, reportRequest);
+    }
+
+    @DeleteMapping("/{reportId}")
+    public void deleteReport(@PathVariable Long reportId) {
+        reportService.deleteReport(reportId);
+    }
 }
