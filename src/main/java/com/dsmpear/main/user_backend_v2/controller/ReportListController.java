d78708537cee2c941f54ed301854cb5262453493
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-04-23 12:09:20 +0900

    [ADD] Controller

--- src/main/java/com/dsmpear/main/user_backend_v2/controller/ReportListController.java
+++ src/main/java/com/dsmpear/main/user_backend_v2/controller/ReportListController.java
@@ -1,12 +1,28 @@
 package com.dsmpear.main.user_backend_v2.controller;
 
+import com.dsmpear.main.user_backend_v2.entity.report.enums.Field;
+import com.dsmpear.main.user_backend_v2.entity.report.enums.Grade;
+import com.dsmpear.main.user_backend_v2.entity.report.enums.Type;
+import com.dsmpear.main.user_backend_v2.payload.response.ReportListResponse;
+import com.dsmpear.main.user_backend_v2.service.report.ReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/report/filter")
 @RestController
 public class ReportListController {
+    private final ReportService reportService;
 
+    @GetMapping
+    public ReportListResponse getReportList(@RequestParam(required = false) Field field,
+                                            @RequestParam(required = false) Type type,
+                                            @RequestParam Grade grade,
+                                            Pageable page) {
+        return reportService.getReportList(page,type,field,grade);
+    }
 }
--- src/main/java/com/dsmpear/main/user_backend_v2/controller/SearchController.java
+++ src/main/java/com/dsmpear/main/user_backend_v2/controller/SearchController.java
@@ -1,12 +1,25 @@
 package com.dsmpear.main.user_backend_v2.controller;
 
+import com.dsmpear.main.user_backend_v2.payload.response.ReportListResponse;
+import com.dsmpear.main.user_backend_v2.service.search.SearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/search")
 @RequiredArgsConstructor
 @RestController
 public class SearchController {
+
+    private final SearchService searchService;
+
+    @GetMapping("/report")
+    public ReportListResponse reportListResponse(@RequestParam("keyword") String keyword,
+                                                 Pageable pageable) {
+        return searchService.searchReport(pageable, keyword);
+    }
 
 }
