9feb3c94332e2688dc2d25b3962b30ed3d1deea7
이승윤 <yyuunn17@naver.com>
2021-04-24 15:12:36 +0900

    [TEST] 마이페이지에서 보고서 목록 확인하기 test

--- src/test/java/com/dsmpear/main/user_backend_v2/MyPageControllerTest.java
+++ src/test/java/com/dsmpear/main/user_backend_v2/MyPageControllerTest.java
@@ -1,20 +1,11 @@
 package com.dsmpear.main.user_backend_v2;
 
-import com.dsmpear.main.user_backend_v2.entity.language.Language;
 import com.dsmpear.main.user_backend_v2.entity.language.LanguageRepository;
-import com.dsmpear.main.user_backend_v2.entity.member.Member;
 import com.dsmpear.main.user_backend_v2.entity.member.MemberRepository;
-import com.dsmpear.main.user_backend_v2.entity.report.Report;
 import com.dsmpear.main.user_backend_v2.entity.report.ReportRepository;
 import com.dsmpear.main.user_backend_v2.entity.report.enums.Access;
-import com.dsmpear.main.user_backend_v2.entity.report.enums.Field;
-import com.dsmpear.main.user_backend_v2.entity.report.enums.Grade;
-import com.dsmpear.main.user_backend_v2.entity.report.enums.Type;
-import com.dsmpear.main.user_backend_v2.entity.reporttype.ReportType;
 import com.dsmpear.main.user_backend_v2.entity.reporttype.ReportTypeRepository;
-import com.dsmpear.main.user_backend_v2.entity.user.User;
 import com.dsmpear.main.user_backend_v2.entity.user.UserRepository;
-import com.dsmpear.main.user_backend_v2.exception.UserNotFoundException;
 import com.dsmpear.main.user_backend_v2.payload.request.SetSelfIntroRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
@@ -25,15 +16,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -46,52 +32,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MyPageControllerTest {
 
     @Autowired
-    private WebApplicationContext context;
-
-    @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private MemberRepository memberRepository;
 
     @Autowired
-    private ReportRepository reportRepository;
-
-    @Autowired
-    private ReportTypeRepository reportTypeRepository;
-
-    @Autowired
-    private LanguageRepository languageRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private BasicTestSupport basicTestSupport;
 
     private MockMvc mvc;
 
     @BeforeEach
     public void setUp() throws Exception {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .build();
+        mvc = basicTestSupport.setUp();
 
-        userRepository.save(
-                User.builder()
-                        .email("test@dsm.hs.kr")
-                        .name("홍길동")
-                        .password(passwordEncoder.encode("1111"))
-                        .authStatus(true)
-                        .selfIntro("lalalala")
-                        .build()
-        );
-        userRepository.save(
-                User.builder()
-                        .email("tset@dsm.hs.kr")
-                        .name("고길동")
-                        .password(passwordEncoder.encode("1111"))
-                        .authStatus(true)
-                        .selfIntro("lalalala")
-                        .build()
-        );
+        basicTestSupport.createUser("test@dsm.hs.kr");
+        basicTestSupport.createReport("title_for_every", true, true, Access.EVERY);
+        basicTestSupport.createReport("title_for_not_shown", false, true, Access.EVERY);
     }
 
     @AfterEach
@@ -137,74 +94,10 @@ class MyPageControllerTest {
     @Test
     @WithMockUser(value = "test@dsm.hs.kr", password = "1111")
     public void getReports() throws Exception {
-        write_report("test@dsm.hs.kr");
-        write_report("test@dsm.hs.kr");
-
         mvc.perform(get("/user/profile/report")
                 .param("size","6")
                 .param("page", "0"))
                 .andExpect(status().isOk()).andDo(print());
-    }
-
-    private Report write_report(String email) {
-        User user = userRepository.findById(email)
-                .orElseThrow(UserNotFoundException::new);
-
-        Report report = reportRepository.save(
-                Report.builder()
-                        .title("hello")
-                        .description("hihello")
-                        .isSubmitted(false)
-                        .isAccepted(false)
-                        .comment("반환합니다")
-                        .github("https://github.com")
-                        .teamName("룰루랄라")
-                        .build()
-        );
-
-        Member member = memberRepository.save(
-                Member.builder()
-                        .report(report)
-                        .user(user)
-                        .build()
-        );
-
-        ReportType reportType = reportTypeRepository.save(
-                ReportType.builder()
-                    .reportId(report.getId())
-                    .type(Type.CIRCLES)
-                    .access(Access.ADMIN)
-                    .field(Field.WEB)
-                    .grade(Grade.GRADE2)
-                    .report(report)
-                    .build()
-        );
-
-        Language language1 = languageRepository.save(
-                Language.builder()
-                        .language("C")
-                        .report(report)
-                        .build()
-        );
-
-        Language language2 = languageRepository.save(
-                Language.builder()
-                        .language("JAVA")
-                        .report(report)
-                        .build()
-        );
-
-        List<Language> languages = List.of(language1,language2);
-
-        report = reportRepository.save(
-                Report.builder()
-                        .members((List<Member>)member)
-                        .reportType(reportType)
-                        .languages(languages)
-                        .build()
-        );
-
-        return report;
     }
 
 }
