07f4cf19033b0cb4621154b326687d0008c74101
이승윤 <yyuunn17@naver.com>
2021-04-24 23:23:31 +0900

    [FEAT] 프로필 페이지에서 보고서 목록보기 api

--- src/main/java/com/dsmpear/main/user_backend_v2/service/profile/ProfileServiceImpl.java
+++ src/main/java/com/dsmpear/main/user_backend_v2/service/profile/ProfileServiceImpl.java
@@ -1,21 +1,32 @@
 package com.dsmpear.main.user_backend_v2.service.profile;
 
+import com.dsmpear.main.user_backend_v2.entity.member.Member;
+import com.dsmpear.main.user_backend_v2.entity.member.MemberRepository;
+import com.dsmpear.main.user_backend_v2.entity.report.Report;
 import com.dsmpear.main.user_backend_v2.entity.report.ReportRepository;
+import com.dsmpear.main.user_backend_v2.entity.report.enums.Access;
+import com.dsmpear.main.user_backend_v2.entity.report.repository.ReportCustomRepositoryImpl;
 import com.dsmpear.main.user_backend_v2.entity.user.User;
 import com.dsmpear.main.user_backend_v2.entity.user.UserRepository;
 import com.dsmpear.main.user_backend_v2.exception.UserNotFoundException;
 import com.dsmpear.main.user_backend_v2.payload.response.ProfilePageResponse;
+import com.dsmpear.main.user_backend_v2.payload.response.ProfileReportResponse;
 import com.dsmpear.main.user_backend_v2.payload.response.ProfileReportsResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ProfileServiceImpl implements ProfileService {
 
     private final UserRepository userRepository;
-    private final ReportRepository reportRepository;
+    private final MemberRepository memberRepository;
+    private final ReportCustomRepositoryImpl reportCustomRepository;
 
     @Override
     public ProfilePageResponse getProfile(String email) {
@@ -32,7 +43,28 @@ public class ProfileServiceImpl implements ProfileService {
 
     @Override
     public ProfileReportsResponse getReport(String userEmail, Pageable page) {
-        return null;
+
+        User user = userRepository.findById(userEmail)
+                .orElseThrow(UserNotFoundException::new);
+        Page<Report> reportPage = reportCustomRepository.findAllByMembersContainsAndIsAcceptedAndIsSubmittedTrueAndReportTypeAccessOrderByReportIdDesc(user, Access.EVERY, page);
+        List<ProfileReportResponse> reportResponses = new ArrayList<>();
+
+        for(Report report : reportPage) {
+            reportResponses.add(
+                    ProfileReportResponse.builder()
+                            .reportId(report.getId())
+                            .title(report.getTitle())
+                            .createdAt(report.getCreatedAt())
+                            .type(report.getReportType().getType())
+                            .build()
+            );
+        }
+
+        return ProfileReportsResponse.builder()
+                .totalElements(reportPage.getTotalElements())
+                .totalPages(reportPage.getTotalPages())
+                .profileReportResponses(reportResponses)
+                .build();
     }
 
 }
