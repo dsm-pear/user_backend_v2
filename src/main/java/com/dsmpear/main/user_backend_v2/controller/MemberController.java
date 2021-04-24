3639328fb6c9145556f92eb60af96877f5f50359
이승윤 <yyuunn17@naver.com>
2021-04-24 13:20:04 +0900

    [ADD] MemberController

--- src/main/java/com/dsmpear/main/user_backend_v2/controller/MemberController.java
+++ src/main/java/com/dsmpear/main/user_backend_v2/controller/MemberController.java
@@ -1,11 +1,27 @@
 package com.dsmpear.main.user_backend_v2.controller;
 
+import com.dsmpear.main.user_backend_v2.payload.request.MemberRequest;
+import com.dsmpear.main.user_backend_v2.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/member")
 @RestController
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addMember(@RequestBody MemberRequest memberRequest) {
+        memberService.addMember(memberRequest);
+    }
+
+    @DeleteMapping("/{memberId}")
+    public void deleteMember(@PathVariable Integer memberId) {
+        memberService.deleteMember(memberId);
+    }
 
 }
