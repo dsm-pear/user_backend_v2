ad2a59a4680f5a87e9cf1eca1c0720f020a79a2f
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-05-02 13:59:47 +0900

    [UPDATE] EmbeddedRedisConfig to better

--- src/main/java/com/dsmpear/main/user_backend_v2/config/EmbeddedRedisConfig.java
+++ src/main/java/com/dsmpear/main/user_backend_v2/config/EmbeddedRedisConfig.java
@@ -7,6 +7,9 @@ import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Configuration
 @Profile("test")
@@ -18,9 +21,17 @@ public class EmbeddedRedisConfig {
     int redisPort;
 
     @PostConstruct
-    public void runRedis() {
+    public void runRedis() throws IOException {
         if(redisServer == null) {
-            redisServer = new RedisServer(redisPort);
+            if(getProcess(redisPort) == -1) {
+                redisServer = new RedisServer(redisPort);
+            } else {
+                int port = 10000;
+                while(getProcess(port) != -1) {
+                    port++;
+                }
+                redisServer = new RedisServer(port);
+            }
             redisServer.start();
         }
     }
@@ -29,5 +40,22 @@ public class EmbeddedRedisConfig {
     public void stopRedis() {
         if(redisServer != null)
             redisServer.stop();
+    }
+
+    private int getProcess(int port) throws IOException {
+        Process ps = new ProcessBuilder("cmd", "/c", "netstat -a -o").start();
+        BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.contains(":" + port)) {
+                while (line.contains("  ")) {
+                    line = line.replaceAll("  ", " ");
+                }
+                int pid = Integer.parseInt(line.split(" ")[5]);
+                ps.destroy();
+                return pid;
+            }
+        }
+        return -1;
     }
 }
