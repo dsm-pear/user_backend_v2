c73de18ea3193ad4f90f79e7fbd529d6797bb2b6
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-05-02 13:47:16 +0900

    [DELETE] member

--- src/main/java/com/dsmpear/main/user_backend_v2/service/member/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.dsmpear.main.user_backend_v2.service.member;
-
-import com.dsmpear.main.user_backend_v2.payload.request.MemberRequest;
-
-public interface MemberService {
-    void addMember(MemberRequest memberRequest);
-    void deleteMember(Integer memberId);
-}
